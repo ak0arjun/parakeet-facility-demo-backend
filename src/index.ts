e756c226748d2c03d47c27c05ae195d3a817f72d
ak0arjun <arjunkapoor.nsit@gmail.com>
2024-12-16 09:11:01 +0530

    add cors permission

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { Hono } from 'hono'
 import { facilityRouter } from './modules/facility/facility.module'
 import { HTTPException } from 'hono/http-exception';
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+ const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS,PATCH,DELETE",
+    "Access-Control-Max-Age": "86400",
+}
+
+app.use('/facility', cors(
+{
+  origin:'*',
+  allowMethods: ['POST', 'GET', 'OPTIONS','PATCH','DELETE'],
+}
+));
 app.route('/facility', facilityRouter);
 
 app.get('/', (c) => {
